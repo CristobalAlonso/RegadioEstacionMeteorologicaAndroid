22008e4aed053b2dc4491ad26122b8048bf20c98
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-10-30 22:35:19 -0300

    base conection to mqtt

--- app/src/main/java/com/example/testandroidmqtt/MainActivity.java
+++ app/src/main/java/com/example/testandroidmqtt/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -16,25 +19,60 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private String clientId, topic;
-    MqttAndroidClient client;
+    private String clientId, topic, swtText;
+    private MqttAndroidClient client;
+
+    private TextView textView;
+    private Switch swt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //objestos del view
+        textView = findViewById(R.id.textTest);
+        swt = findViewById(R.id.swtTest);
+
+        swtText="";
         clientId ="";
         init();
+
+        swt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(swt.isChecked())
+                {
+                    try
+                    {
+                        MqttMessage message = new MqttMessage(("ON").getBytes());
+                        client.publish("led/01", message);
+                    }
+                    catch (MqttException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }else
+                {
+                    try
+                    {
+                        MqttMessage message = new MqttMessage(("OFF").getBytes());
+                        client.publish("led/01", message);
+                    }
+                    catch (MqttException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     public void init(){
-        clientId = "xxx";
-        topic = "test/01";
+        clientId = "crisss"; //los usuarios id deben ser todos distintos
+        topic = "led/01";
 
-        client=
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
-                        clientId);
+        client=new MqttAndroidClient(this.getApplicationContext(), "tcp://68.183.119.177:1883",clientId);
         conect();
     }
 
@@ -67,20 +105,38 @@ public class MainActivity extends AppCompatActivity {
         try
         {
             client.subscribe(topic, 0);
-            client.setCallback(new MqttCallback() {
+            client.setCallback(new MqttCallback()
+            {
                 @Override
-                public void connectionLost(Throwable cause) {
+                public void connectionLost(Throwable cause)
+                {
 
                 }
 
                 @Override
-                public void messageArrived(String topic, MqttMessage message) throws Exception {
-                    Log.d(TAG, "topic: "+ topic);
-                    Log.d(TAG, "msg: "+ new String(message.getPayload()));
+                public void messageArrived(String topic, MqttMessage message) throws Exception
+                {
+                    if(topic.matches("led/01"))
+                    {
+                        swtText = new String(message.getPayload());
+                        if(swtText.matches("ON"))
+                        {
+                            Log.d(TAG, "true switch");
+                            textView.setText("true switch");
+                            swt.setChecked(true);
+                        }
+                        if(swtText.matches("OFF"))
+                        {
+                            Log.d(TAG, "false switch");
+                            textView.setText("false switch");
+                            swt.setChecked(false);
+                        }
+                    }
                 }
 
                 @Override
-                public void deliveryComplete(IMqttDeliveryToken token) {
+                public void deliveryComplete(IMqttDeliveryToken token)
+                {
 
                 }
             });
